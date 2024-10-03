78d12bec75f342708e7a7e844bcc21cf6a7077cc
Abugre Abayeta <abayetaabugre@gmail.com>
2024-10-03 15:44:05 +0000

    Added

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,11 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddTodo = () => {
   const navigate = useNavigate();
 
-  const saveTodo = (event) => {
+  const saveTodo = async (event) => {
     event.preventDefault();
+    // collect form input
+    const formData = new FormData(event.target);
     // post data todo api
+    await axios.post('https://todo-api-olfc.onrender.com/todos', {
+      title: formData.get('todo')
+    });
     // goto the hompage
     navigate("/");
   };
@@ -14,7 +20,7 @@ const AddTodo = () => {
     <div>
       <h1>Add A New Todo</h1>
       <form onSubmit={saveTodo}>
-        <input type="text" placeholder="Enter your Todo" required />
+        <input type="text" placeholder="Enter your todo" required name="todo"/>
         <button>Submit</button>
       </form>
     </div>
